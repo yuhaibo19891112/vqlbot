052cb25beb75b921c25b5b10c36d606f71af4f87
yuhaibo <yuhaibo19891112@gmail.com>
2023-01-09 22:36:14 +0800

    init

--- config/config.go
+++ config/config.go
@@ -9,8 +9,6 @@ import (
 
 // Configuration 项目配置
 type Configuration struct {
-	// gtp apikey
-	ApiKey string `json:"api_key"`
 	// 自动通过好友
 	AutoPass bool `json:"auto_pass"`
 }
@@ -37,11 +35,7 @@ func LoadConfig() *Configuration {
 		}
 
 		// 如果环境变量有配置，读取环境变量
-		ApiKey := os.Getenv("ApiKey")
 		AutoPass := os.Getenv("AutoPass")
-		if ApiKey != "" {
-			config.ApiKey = ApiKey
-		}
 		if AutoPass == "true" {
 			config.AutoPass = true
 		}
